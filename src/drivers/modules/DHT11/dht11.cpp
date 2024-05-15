00f21fbc0aab349460f1ef87bdaec5aca2d658c7
geraldombuthia <geraldombuthia@gmail.com>
2024-05-15 19:00:43 +0300

    Set the DHTPIN to 3 DHT works fine. WIll need to be converted into a class later on

--- src/drivers/modules/DHT11/dht11.cpp
+++ src/drivers/modules/DHT11/dht11.cpp
@@ -1,70 +1,78 @@
-#include "dht11.h"
+// #include "dht11.h"
+
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define MAXTIMINGS	85
+#define DHTPIN		3
 
 
-// int		dht11_dat[5] = { 0, 0, 0, 0, 0 };
-// double	temp;
-// double	humidity;
 
-// int iread_dht11_dat()
-// {
-// 	uint8_t		laststate = HIGH;
-// 	uint8_t		counter = 0;
-// 	uint8_t		j = 0, i;
+int		dht11_dat[5] = { 0, 0, 0, 0, 0 };
+double	temp;
+double	humidity;
 
-// 	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+int read_dht11_dat()
+{
+	uint8_t		laststate = HIGH;
+	uint8_t		counter = 0;
+	uint8_t		j = 0, i;
 
-// 	pinMode(DHTPIN, OUTPUT);
-// 	digitalWrite(DHTPIN, LOW);
-// 	delay(18);
-// 	digitalWrite(DHTPIN, HIGH);
-// 	delayMicroseconds(40);
-// 	pinMode(DHTPIN, INPUT);
+	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
-// 	for (i = 0; i < MAXTIMINGS; i++)
-// 	{
-// 		counter = 0;
-// 		while (digitalRead(DHTPIN) == laststate)
-// 		{
-// 			counter++;
-// 			delayMicroseconds(1);
-// 			if (counter == 255)
-// 				break;
-// 		}
-// 		laststate = digitalRead(DHTPIN);
+	pinMode(DHTPIN, OUTPUT);
+	digitalWrite(DHTPIN, LOW);
+	delay(18);
+	digitalWrite(DHTPIN, HIGH);
+	delayMicroseconds(40);
+	pinMode(DHTPIN, INPUT);
 
-// 		if (counter == 255)
-// 			break;
+	for (i = 0; i < MAXTIMINGS; i++)
+	{
+		counter = 0;
+		while (digitalRead(DHTPIN) == laststate)
+		{
+			counter++;
+			delayMicroseconds(1);
+			if (counter == 255)
+				break;
+		}
+		laststate = digitalRead(DHTPIN);
 
-// 		if ((i >= 4) && (i % 2 == 0))
-// 		{
-// 			dht11_dat[j / 8] <<= 1;
-// 			if (counter > 64)
-// 				dht11_dat[j / 8] |= 1;
-// 			j++;
-// 		}
-// 	}
+		if (counter == 255)
+			break;
 
-// 	if ((j >= 40) && (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
-// 	{
-// 		temp = dht11_dat[2] + (dht11_dat[3] / 10);
-// 		humidity = dht11_dat[0] + (dht11_dat[1] / 10);
-// 		return (1);
-// 	}
-// 	else
-// 		return (0);
-// }
+		if ((i >= 4) && (i % 2 == 0))
+		{
+			dht11_dat[j / 8] <<= 1;
+			if (counter > 64)
+				dht11_dat[j / 8] |= 1;
+			j++;
+		}
+	}
 
-// int main(int argc, char *argv[])
-// {
-// 	int ret = 0;
+	if ((j >= 40) && (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
+	{
+		temp = dht11_dat[2] + (dht11_dat[3] / 10);
+		humidity = dht11_dat[0] + (dht11_dat[1] / 10);
+		return (1);
+	}
+	else
+		return (0);
+}
 
-// 	if (wiringPiSetup() == -1)
-// 		exit(1);
-// 	while (ret == 0)
-// 	{
-// 		ret = read_dht11_dat();
-// 		delay(100);
-// 	}
-// 	printf("Humidity: %.1f%%\nTemperature: %.1f C\n", humidity, temp);
-// 	return(0);
-// }
+int main(int argc, char *argv[])
+{
+	int ret = 0;
+
+	if (wiringPiSetup() == -1)
+		exit(1);
+	while (ret == 0)
+	{
+		ret = read_dht11_dat();
+		delay(100);
+	}
+	printf("Humidity: %.1f%%\nTemperature: %.1f C\n", humidity, temp);
+	return(0);
+}
