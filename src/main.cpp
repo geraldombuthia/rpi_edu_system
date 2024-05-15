4adf1e922b1c15d488c93f43937fd94da3c86238
geraldombuthia <geraldombuthia@gmail.com>
2024-05-15 19:21:53 +0300

    Fixed the DHTPIn in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,63 +5,60 @@
 #include "mcp3008.h"
 #include <wiringPi.h>
 
+#define MAXTIMINGS 85
+#define DHTPIN 3
 
-
-#define MAXTIMINGS	85
-#define DHTPIN		22
-
-
-int		dht11_dat[5] = { 0, 0, 0, 0, 0 };
-double	dht_temp;
-double	dht_humidity;
+int dht11_dat[5] = {0, 0, 0, 0, 0};
+double dht_temp;
+double dht_humidity;
 
 int read_dht11_dat()
 {
-	uint8_t		laststate = HIGH;
-	uint8_t		counter = 0;
-	uint8_t		j = 0, i;
+    uint8_t laststate = HIGH;
+    uint8_t counter = 0;
+    uint8_t j = 0, i;
 
-	dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
-	pinMode(DHTPIN, OUTPUT);
-	digitalWrite(DHTPIN, LOW);
-	delay(18);
-	digitalWrite(DHTPIN, HIGH);
-	delayMicroseconds(40);
-	pinMode(DHTPIN, INPUT);
+    pinMode(DHTPIN, OUTPUT);
+    digitalWrite(DHTPIN, LOW);
+    delay(18);
+    digitalWrite(DHTPIN, HIGH);
+    delayMicroseconds(40);
+    pinMode(DHTPIN, INPUT);
 
-	for (i = 0; i < MAXTIMINGS; i++)
-	{
-		counter = 0;
-		while (digitalRead(DHTPIN) == laststate)
-		{
-			counter++;
-			delayMicroseconds(1);
-			if (counter == 255)
-				break;
-		}
-		laststate = digitalRead(DHTPIN);
+    for (i = 0; i < MAXTIMINGS; i++)
+    {
+        counter = 0;
+        while (digitalRead(DHTPIN) == laststate)
+        {
+            counter++;
+            delayMicroseconds(1);
+            if (counter == 255)
+                break;
+        }
+        laststate = digitalRead(DHTPIN);
 
-		if (counter == 255)
-			break;
+        if (counter == 255)
+            break;
 
-		if ((i >= 4) && (i % 2 == 0))
-		{
-			dht11_dat[j / 8] <<= 1;
-			if (counter > 64)
-				dht11_dat[j / 8] |= 1;
-			j++;
-		}
-	}
+        if ((i >= 4) && (i % 2 == 0))
+        {
+            dht11_dat[j / 8] <<= 1;
+            if (counter > 64)
+                dht11_dat[j / 8] |= 1;
+            j++;
+        }
+    }
 
-	if ((j >= 40) && (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
-	{
-		dht_temp = dht11_dat[2] + (dht11_dat[3] / 10);
-		dht_humidity = dht11_dat[0] + (dht11_dat[1] / 10);
-		return (1);
-	}
-	else
-		return (0);
+    if ((j >= 40) && (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF)))
+    {
+        dht_temp = dht11_dat[2] + (dht11_dat[3] / 10);
+        dht_humidity = dht11_dat[0] + (dht11_dat[1] / 10);
+        return (1);
+    }
+    else
+        return (0);
 }
 
 int main()
@@ -81,21 +78,20 @@ int main()
 
     mcp3008.connect();
 
-
-
     int ret = 0;
 
     if (wiringPiSetup() == -1)
         exit(1);
 
     // Now you can access the measurements
-    float temperature = measurements.temperature; // in degrees Celsius
-    float pressure = measurements.pressure;       // in hPa
-    float humidity = measurements.humidity;       // in %
-    float altitude = measurements.altitude;       // in meters
 
     while (1)
     {
+        float temperature = measurements.temperature; // in degrees Celsius
+        float pressure = measurements.pressure;       // in hPa
+        float humidity = measurements.humidity;       // in %
+        float altitude = measurements.altitude;       // in meters
+
         mpu6050.getGyro(&roll, &pitch, &yaw);
         mpu6050.getAccel(&ax, &ay, &az);
 
@@ -110,6 +106,6 @@ int main()
 
         printf("Temperature: %.2f C Pressure: %.2f hPa Humidity: %.2f %% Altitude: %.2f m\n\n", temperature, pressure, humidity, altitude);
 
-        printf("Channel 1: %d\n", channel1);
+        printf("Channel 1: %d\n\n", channel1);
     }
 }
