cff57807c3f11b74d6ec79ce6c728ebad94fdc9f
Koech <ford.koech.eric@gmail.com>
2024-06-04 00:02:09 +0300

    fix(build): restructed cmake properly

---
+++ src/gui/gui_app.cpp
@@ -0,0 +1,20 @@
+#include "gui_app.h"
+
+
+int GUI::init()
+{
+    // TODO - implement
+    return -1;
+}
+
+GUI* GUI::get_instance()
+{
+    static GUI instance;
+    return &instance;
+}
+
+int GUI::update()
+{
+    // TODO - implement
+    return -1;
+}
